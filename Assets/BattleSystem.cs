91e6c91e67f64ff6e46d4273b5567a72e5e331a4
jaider4419 <jaiderboateng8@gmail.com>
2024-04-26 10:44:06 +0100

    14:29

---
+++ Assets/BattleSystem.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+public class BattleSystem : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public TextMeshProUGUI dialogueText;
+    public BattleState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+
+    void SetupBattle()
+    {
+        GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerUnit = playerGO.GetComponent<Unit>();
+
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogueText.text = "A wild " + enemyUnit.unitName + " approaches...";
+    }
+}
